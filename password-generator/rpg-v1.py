a76ad96b8ef8b41bd9a81bfee597a5cd3ab35b74
unknown <beckhv2@gmail.com>
2023-02-24 23:02:23 -0900

    change comments to docstrings

--- password-generator/rpg-v1.py
+++ password-generator/rpg-v1.py
@@ -1,3 +1,29 @@
+'''
+Title: Random Password Generator, v.1
+Description: Creates a random password from 4 to 15 characters long.
+Developer: Alexander Beck
+Email: beckhv2@gmail.com
+Github: https://github.com/bexcoding
+
+\\......//\\......//\\......//\\......//\\......//\\......//\\......//\\......//
+ \\....//  \\....//  \\....//  \\....//  \\....//  \\....//  \\....//  \\....// 
+\ \\..// /\ \\..// /\ \\..// /\ \\..// /\ \\..// /\ \\..// /\ \\..// /\ \\..// /
+ \ \\// /  \ \\// /  \ \\// /  \ \\// /  \ \\// /  \ \\// /  \ \\// /  \ \\// / 
+> | || | <> | || | <> |                                     > | || | <> | || | <  
+> | || | <> | || | <> | ||                          ||   // > | || | <> | || | <
+ /  /\  \  /  /\  \  /  ||                          ||  //   /  /\  \  /  /\  \ 
+/  /  \  \/  /  \  \/   ||____       ___      ___   || //   /  /  \  \/  /  \  \
+  | <> | || | <> | ||   ||    \\   //   \\  //   \\ ||||    | | <> | || | <> | |
+  | <> | || | <> | ||   ||     || ||____|| ||       || \\   | | <> | || | <> | |
+   \  /  ||  \  /  ||   ||     || ||       ||       ||  \\  |  \  /  ||  \  /  | 
+\\  \/  //\\  \/  //\\  ||____//   \\___//  \\___// ||   \\ \\  \/  //\\  \/  //
+.\\    //..\\    //..\\                                      \\    //..\\    //.
+..\\  //....\\  //....\\  //....\\  //....\\  //....\\  //....\\  //....\\  //..
+...\\//......\\//......\\//......\\//......\\//......\\//......\\//......\\//...
+
+
+'''
+
 import string
 import random
 
@@ -6,10 +32,12 @@ lowers = string.ascii_lowercase
 nums = string.digits
 symbs = string.punctuation
 
-# string -> int
-# given the desired password length, return an int version of the input
-# or if the input is invalid, return the int 8 
 def check_input(user_input):
+    '''
+string -> int
+given the desired password length, return an int version of the input
+or if the input is invalid, return the int 8 
+    '''
     try:
         user_input = int(user_input) 
     except:
@@ -20,25 +48,31 @@ def check_input(user_input):
         else:
             return 8
 
-# -> string
-# selects a random character type and returns a random element from it
 def random_item():
+    '''
+() -> string
+selects a random character type and returns a random element from it
+    '''
     choice_type = random.choice([uppers, lowers, nums, symbs])
     return random.choice(choice_type)
 
-# int, string -> string
-# given the desired length of the password (n) and an empty string,
-# returns a password with the chosen length
 def create_password(n, password):
+    '''
+int, string -> string
+given the desired length of the password (n) and an empty string,
+returns a password with the chosen length
+    '''
     if n <= 0:
         return password
     else:
         return create_password(n - 1, random_item() + password)
 
-# string, int, int, int, int -> list of ints
-# given the preliminary password and an accumulator for each group of character
-# type, returns a list that shows how much each category was used
 def pass_checker(password, up, low, num, sym):
+    '''
+string, int, int, int, int -> list of ints
+given the preliminary password and an accumulator for each group of character
+type, returns a list that shows how much each category was used
+    '''
     if password == "":
         return [up, low, num, sym]
     else:
@@ -51,11 +85,13 @@ def pass_checker(password, up, low, num, sym):
         elif password[0] in symbs:
             return pass_checker(password[1:], up, low, num, sym + 1)
 
-# string, list of ints -> string
-# given the preliminary password and a list that shows how many times each
-# category is used, returns the original password if all categories are present
-# or adds any desired character that is missing
 def modify(mod, check_list):
+    '''
+string, list of ints -> string
+given the preliminary password and a list that shows how many times each
+category is used, returns the original password if all categories are present
+or adds any desired character that is missing
+    '''
     if check_list[0] == 0:
         mod += random.choice(uppers)
 
